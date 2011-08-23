025332b6f00b8a7dad42928ee20d2b8f5a818130
edewit@232.43.168.192.in-addr.arpa <edewit@232.43.168.192.in-addr.arpa>
2011-08-23 18:29:18 +0200

    unused import

--- src/test/java/nl/erikjan/generators/testdata/framework/integration/Manager.java
+++ src/test/java/nl/erikjan/generators/testdata/framework/integration/Manager.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Lob;
+import java.util.List;
 
 /**
  * @author edewit

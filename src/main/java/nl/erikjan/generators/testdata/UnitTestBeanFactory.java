c9bae2d6db2a1aa2a719db3a0a8f662cede45c88
edewit@erik-jan-mbp.canoo.com <edewit@erik-jan-mbp.canoo.com>
2011-09-01 14:28:54 +0200

    temporally added method to create beans from method

--- src/main/java/nl/erikjan/generators/testdata/UnitTestBeanFactory.java
+++ src/main/java/nl/erikjan/generators/testdata/UnitTestBeanFactory.java
@@ -1,17 +1,5 @@
 package nl.erikjan.generators.testdata;
 
-import org.aopalliance.aop.Advice;
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.aop.Advisor;
-import org.springframework.aop.MethodBeforeAdvice;
-import org.springframework.aop.Pointcut;
-import org.springframework.aop.framework.ProxyFactory;
-import org.springframework.aop.framework.ProxyFactoryBean;
-import org.springframework.aop.support.ControlFlowPointcut;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.aop.support.JdkRegexpMethodPointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -31,6 +19,14 @@ public class UnitTestBeanFactory {
 
     public static <T> T createBeanInstance(Class<T> beanClass) {
         return beanFactory.instantiateBean(beanClass);
+    }
+
+    public static Object createBeanInstances(Method method) {
+        try {
+            return beanFactory.instantiateBeans(method);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public static <T> T createService(Class<T> serviceClass) {
